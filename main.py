ca26a85602c20db8873836d32058293bcc67010f
Nandu-R-Nair <nandurnair146@gmail.com>
2021-10-04 22:14:33 +0530

    second commit

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ prop = "Previous Close"
 print(r.status_code==200)
 t = r.text
 ind = (t.index("Previous Close"))
+ind2 = (t.index(("Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)")))
+print(ind2)
 redtex = t[ind:].split("</span>")[1]
 val = redtex.split(">")[-1]
 print(val)
+redtex2 = t[ind2:ind2+67].split('49">')[1]
+print(redtex2)
