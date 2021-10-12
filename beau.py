aa5f4c031a3f28703c66ea58efdc1d9130e0ccbd
Nandu-R-Nair <nandurnair146@gmail.com>
2021-10-12 12:04:55 +0530

    beutifulsoup commit 2

--- beau.py
+++ beau.py
@@ -7,6 +7,12 @@ t = response.text
 
 soup = BeautifulSoup(t,features="html.parser")
 
-trs = soup.find_all("tr")
+trs = soup.find_all("tbody")
+#for i in range(0,7):
+tr2 = trs[0].find_all("td")
+for i in range(0,14):
 
-print(trs[0].contents[1])
+    print(tr2[i].text)
+#print(trs[0].contents[0].span.text)
+#print(trs[0].contents[0].td.text)
+#print(trs[1].tr.contents[0].text)
